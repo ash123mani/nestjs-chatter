303539c8f7ce6cd5265ed1319b7ecbc9a5d4819c
Ashutosh <ashutosh.t@onusworks.com>
2024-05-20 00:25:11 +0530

    added socket io client

--- workspaces/client/app/chat/machines/wChatMachine.ts
+++ workspaces/client/app/chat/machines/wChatMachine.ts
@@ -1,18 +1,68 @@
-import { createMachine, assign } from "xstate";
+import { assign, setup } from "xstate";
+import {
+  Message,
+  User
+} from '@chatter-pwa/shared';
 
-export const wsChatMachine = createMachine({
+export enum ChatMachineState {
+  Connected = 'CONNECTED',
+  Disconnected = 'DISCONNECTED'
+}
+export enum ChatMachineEvent {
+  Connect = 'CONNECT',
+  Disconnect = 'DISCONNECT',
+  SendMessage = 'SEND_MESSAGE'
+}
+
+// @ts-ignore
+export const wsChatMachine = setup({
+  types: {
+      context: {} as ChatMachineContext,
+    events: {} as ChatMachineEvents
+  },
+  actions: {
+    updateLoggedInUser: assign({
+      user: ({ context }) => {
+        const currentUser = JSON.parse(sessionStorage.getItem('user') ?? '{}') as User;
+        if (currentUser.userId) return currentUser
+        else return null;
+      }
+    }),
+  }
+}).createMachine({
   id: "chatMachine",
+  entry: [{ type: 'updateLoggedInUser' }],
   initial: "disconnected",
   context: {
     messages: [],
     user: null
   },
   states: {
-    connected: {
-      on: { DISCONNECT: 'disconnected' }
+    [ChatMachineState.Connected]: {
+      on: {
+        [ChatMachineEvent.Disconnect]: ChatMachineState.Disconnected,
+        [ChatMachineEvent.SendMessage]: {
+          actions: assign({
+            // messages: ({ event, context }: { event: any, context: ChatMachineContext }) => context.messages.push((event.message as Message))
+          })
+        }
+      }
     },
-    disconnected: {
-      on: { CONNECT: 'connected' }
+    [ChatMachineState.Disconnected]: {
+      on: { [ChatMachineEvent.Connect]: ChatMachineState.Connected }
     }
   }
 })
+
+type ChatMachineSchema  = {
+  states: {
+    connected: {},
+    disconnected: {}
+  }
+}
+type ChatMachineContext = {
+  messages: Message[],
+  user: User | null
+}
+type ChatMachineEvents = { type: ChatMachineEvent } | { type: ChatMachineEvent.SendMessage, message: Message }
+
--- workspaces/client/app/chat/page.tsx
+++ workspaces/client/app/chat/page.tsx
@@ -1,22 +1,42 @@
 "use client"
 import { Box, Text } from '@chakra-ui/react';
 import { useActor } from '@xstate/react';
+import { io, Socket } from 'socket.io-client'
 import { wsChatMachine } from '@/app/chat/machines';
 
 import { Header } from './components/Header'
 import { Messages } from './components/Messages'
 import { MessageForm } from './components/MessageForm'
 import { LoginForm } from './components/LoginForm'
+import { useEffect } from 'react';
+import { ServerToClientEvents, ClientToServerEvents } from '@chatter-pwa/shared';
+import { ChatMachineEvent, ChatMachineState } from '@/app/chat/machines/wChatMachine';
 
-// TODO: Add the ChatLayout and LoginFormLayout
-// TODO: Fix the type for the wsChatMachine
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
 function Chat() {
-  const [state, send] = useActor(wsChatMachine);
-  let messages: any = [];
-  let user: any = {
-    userId: ''
-  };
-  const isConnected = state.matches("connected");
+  const [state, send] = useActor<typeof wsChatMachine>(wsChatMachine);
+  const { messages, user } = state.context;
+  const isConnected = state.matches(ChatMachineState.Connected);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      send({ type: ChatMachineEvent.Connect })
+    });
+
+    socket.on('disconnect', () => {
+      send({ type: ChatMachineEvent.Disconnect })
+    });
+
+    socket.on('chat', () => {
+      // Send the SendMessage Event
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('chat');
+    };
+  }, [send]);
 
   return (
     <Box>
